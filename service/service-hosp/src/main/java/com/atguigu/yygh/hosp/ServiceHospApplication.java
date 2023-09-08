593cb1578b2b8d7e897f80aad0331a488ddf9efc
老马 <3314991589@qq.com>
2023-09-08 21:31:50 +0800

    初始化医院设置结构

---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author malinyan
+ * @Date 2023/9/8
+ * @Time 21:29
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
