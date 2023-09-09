b0dd4727577616800d6f7919fa85961f816a541a
老马 <3314991589@qq.com>
2023-09-09 15:36:41 +0800

    完成统一返回结果

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,5 +1,6 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
 import io.swagger.annotations.Api;
@@ -25,16 +26,20 @@ public class HospitalSetController {
     //1 查询医院设置表所有信息
     @ApiOperation(value = "获取所有医院设置")
     @GetMapping("findAll")
-    public List<HospitalSet> findAllHospitalSet(){
+    public Result findAllHospitalSet(){
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
     }
 
     //2逻辑删除医院设置
     @ApiOperation(value = "逻辑删除医院设置")
     @DeleteMapping("{id}")
-    public boolean removeHospSet(@PathVariable Long id){
+    public Result removeHospSet(@PathVariable Long id){
         boolean flag = hospitalSetService.removeById(id);
-        return flag;
+        if (flag){
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
     }
 }
